e1093e74007f8b510a97c505e627fcf9ee13d3d5
Nelissen <toon@appsaloon.be>
2013-11-06 10:29:06 +0100

    Continu array van curve objects

--- parser/parser.js
+++ parser/parser.js
@@ -3,11 +3,21 @@ if (process.argv.length < 3) {
   process.exit(1);
 }
 
+Array.prototype.toFloat=function() {
+	if (this[0] == ''){
+		this.splice(0, 1);
+	}
+  	for (i=0;i<this.length;i++) {
+  		this[i]=parseFloat(this[i]);
+  	}
+  	return this;
+};
+
 var fs = require('fs')
 	, filename = process.argv[2]
 	, XmlStream = require('xml-stream')
 	, paths = new Array()
-	, continuousPath = new Array()
+	, contPaths = new Array()
 	, svgData
 	;
 
@@ -27,68 +37,21 @@ xml.on('endElement: svg', function(item) {
 });
 xml.on('end', function(){
 	// alles in een lijst dumpen
-	paths.unshift(['M'+svgData.width.slice(0,-2)/2+','+svgData.height.slice(0, -2)/2]);
+	paths.unshift(['O'+svgData.width.slice(0,-2)/2+','+svgData.height.slice(0, -2)/2]);
+	
 	for (var i = 0; i < paths.length; i++) {
 		for (var j = 0; j < paths[i].length; j++) {
-			var curveType = paths[i][j].slice(0, 1);
-			var curvePoints = paths[i][j].substr(1).split(",");
-			var nextCurveType = function () {
-				if (paths[i].length == j+1){
-					if( paths.length == i+1){
-						return;
-					} else {
-						return paths[i+1][0].slice(0, 1)
-					}
-				} else {
-					return paths[i][j+1].slice(0, 1)
-				}
-			}();
-			var nextCurvePoints = function () {
-				if (paths[i].length == j+1){
-					if( paths.length == i+1){
-						return;
-					} else {
-						return paths[i+1][0].substr(1).split(",");
-					}
-				} else {
-					return paths[i][j+1].substr(1).split(",");
-				}
-			}();
-			
-						console.log(curveType+ ' ' + curvePoints + ' - ' + nextCurveType +' '+ nextCurvePoints);
-						  
-			switch (curveType) {
-				case 'M':
-					switch (nextCurveType) {
-						case "M":
-							Add line
-						break;
-					}
-				break;
-				case 'c':
-					console.log('curve');
-				break;
-				case 'v':
-					console.log('kleine v');
-				break;
-				case 'V':
-					console.log('grote V');
-				break;
-				case 'h':
-					console.log('kleine horizontal');
-				break;
-				case 'z':
-					console.log('end');
-				break;
-				case 'l':
-					console.log('line');
-				break;
-				case 's':
-					console.log('iets met S');
-				break;
-			}
+			var numbers = paths[i][j].substr(1).replace(/-/gi, ',-');
+			console.log(numbers);
+			var arrayOfNumbers = numbers.split(",");
+			console.log(arrayOfNumbers);
+			contPaths.push({
+				type: paths[i][j].slice(0, 1),
+				points : arrayOfNumbers.toFloat()
+			})
 		}
 	}
+	console.log(contPaths);
 })
 
 
